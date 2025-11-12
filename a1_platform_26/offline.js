dbff2676371c311d569416a0c2fde6f870454bed
s41405 <s41405@bj.ac.th>
2025-11-12 11:05:46 +0700

    Add files via upload

---
+++ a1_platform_26/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1762920033,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bgsky-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/coinbox-sheet0.png",
+		"images/player-sheet0.png",
+		"images/coin2-sheet0.png",
+		"images/coin2-sheet1.png",
+		"images/block.png",
+		"images/sprite-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
